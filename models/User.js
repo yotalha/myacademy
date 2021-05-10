bded7ae3d7b4a1c01eea5d1de17fa97c22bd6f94
yotalha <talha.coder9@gmail.com>
2021-05-10 07:53:20 +0500

    create and read crud

---
+++ models/User.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstName: {
+      type: DataTypes.STRING
+    },
+    lastName: {
+      type: DataTypes.STRING
+    },
+    createdAt: {
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      type: DataTypes.DATE
+    }
+  })
+
+  return User;
+}
