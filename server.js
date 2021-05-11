f4fede4a6897a5a4048dbafe0787db2a2498d231
yotalha <talha.coder9@gmail.com>
2021-05-12 04:12:58 +0500

    testing

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const db = require('./models');
 const userRoutes = require('./routes/user');
 // const {User} = require('./models');
+// const {Course, Class} = require('models');
 
 db.sequelize.sync();
 
@@ -10,6 +11,26 @@ app.use(express.json())
 
 app.use('/users', userRoutes);
 
+// app.post('/classes', async(req, res) => {
+//   try{
+//     const {code, block} = req.body;
+//     const newClass = await Class.create({code, block});
+//     res.send(newClass)
+//   }
+//   catch(err){
+//     res.send('error', err);
+//   }
+// })
+
+// app.get('/classes', async(req, res) => {
+//   try{
+//     const myClass = await Class.findAll();
+//     res.send(myClass);
+//   }
+//   catch(err){
+//     res.send('error', err);
+//   }
+// })
 
 app.listen(3000, () => {
   console.log('listening on port 3000!!!');
