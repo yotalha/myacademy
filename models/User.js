7d7eeb6dad2b822274073d0c36996b1a38724c54
yotalha <talha.coder9@gmail.com>
2021-06-22 18:36:18 +0500

    updated user model, added fullName, email and added allowNull prop

--- models/User.js
+++ models/User.js
@@ -2,10 +2,20 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: {
       type: DataTypes.STRING,
-      unique: true
+      unique: true,
+      allowNull: false
+    },
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
     password: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false
     },
     createdAt: {
       type: DataTypes.DATE
